642c48fe083ed3c6a9c3d0099b3bb43edfe32976
deepsweet <kir@soulshine.in>
2016-01-17 09:32:15 +0700

    :space_invader: testing suite

---
+++ test/lib/index.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+import loader from '../../lib/';
+
+describe('loader', function() {
+    it('is function', function() {
+        assert(typeof loader === 'function');
+    });
+});
