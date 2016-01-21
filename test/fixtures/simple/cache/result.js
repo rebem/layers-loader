64bf2d7d2bb529d470b8119fc52589a7535c2812
deepsweet <kir@soulshine.in>
2016-01-21 12:21:09 +0700

    :wrench: always try to get component type from `['default']` export

--- test/fixtures/simple/cache/result.js
+++ test/fixtures/simple/cache/result.js
@@ -1,2 +1,2 @@
-/* #target */ require('react').createFactory(require('babel-runtime/helpers/interop-require-default')['default'](require('layer-0/target/index.js'))['default']);
-/* #target */ require('react').createFactory(require('babel-runtime/helpers/interop-require-default')['default'](require('layer-0/target/index.js'))['default']);
+/* #target */ require('react').createFactory(require('layer-0/target/index.js')['default']);
+/* #target */ require('react').createFactory(require('layer-0/target/index.js')['default']);
